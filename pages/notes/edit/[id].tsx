516c658562f495e38de4ba57f39860dfcef59fc5
Robbob24 <r.lawrence2491@gmail.com>
2024-03-03 22:17:15 -0500

    5th

--- pages/notes/edit/[id].tsx
+++ pages/notes/edit/[id].tsx
@@ -2,10 +2,67 @@ import { AntdEditInferencer } from "@refinedev/inferencer/antd";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
+import { Typography, Form, Input, InputNumber } from "antd";
+import { Edit, useForm } from "@refinedev/antd";
 
-export default function NotesEdit() {
-  return <AntdEditInferencer hideCodeViewerInProduction/>;
-}
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+export default function ProductEdit() {
+  const { formProps, saveButtonProps, formLoading } = useForm();
+
+  return (
+    <Edit saveButtonProps={saveButtonProps} isLoading={formLoading}>
+      <Form {...formProps} layout="vertical">
+          <Form.Item
+              label="Title"
+              name="title"
+              rules={[
+                  {
+                      required: true,
+                  },
+              ]}
+          >
+              <Input />
+          </Form.Item>
+          <Form.Item
+              label="Content"
+              name="content"
+              rules={[
+                  {
+                      required: true,
+                  },
+              ]}
+          >
+              <TextArea rows={4} />
+          </Form.Item>
+          <Form.Item
+              label="Created By"
+              name="created_by"
+              rules={[
+                  {
+                      required: true,
+                  },
+              ]}
+          >
+              <TextArea  />
+          </Form.Item>
+          <Form.Item
+              label="Created At"
+              name="created_at"
+              rules={[
+                  {
+                      required: true,
+                  },
+              ]}
+          >
+              <Input />
+          </Form.Item>
+      </Form>
+  </Edit>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
   const { authenticated, redirectTo } = await authProvider.check(context);
