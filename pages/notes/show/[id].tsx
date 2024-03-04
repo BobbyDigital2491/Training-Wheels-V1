2315fa1f4583fb444600e48aaaca85795475ba8e
Robbob24 <r.lawrence2491@gmail.com>
2024-03-03 21:50:50 -0500

    4th

--- pages/notes/show/[id].tsx
+++ pages/notes/show/[id].tsx
@@ -1,11 +1,31 @@
-import { AntdShowInferencer } from "@refinedev/inferencer/antd";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
+import { MarkdownField, NumberField, Show, TextField } from "@refinedev/antd";
+import { Typography } from "antd";
+import { useShow } from "@refinedev/core";
+
+const { Title } = Typography;
 
 export default function NotesShow() {
-  return <AntdShowInferencer hideCodeViewerInProduction/>;
-}
+  const { queryResult } = useShow();
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  return (
+    <Show isLoading={isLoading}>
+      <Title level={5}>Title</Title>
+      <TextField value={record?.title ?? ""} />
+      <Title level={5}>Content</Title>
+      <TextField value={record?.content} />
+      <Title level={5}>Created By</Title>
+      <TextField value={record?.created_by} />
+      <Title level={5}>Created At</Title>
+      <MarkdownField value={record?.created_at} />
+    </Show>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
   const { authenticated, redirectTo } = await authProvider.check(context);
