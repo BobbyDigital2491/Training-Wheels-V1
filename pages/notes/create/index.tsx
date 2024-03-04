374381892b043994ed92fe9f56380294f93dc125
Robbob24 <r.lawrence2491@gmail.com>
2024-03-03 22:44:01 -0500

    6th

--- pages/notes/create/index.tsx
+++ pages/notes/create/index.tsx
@@ -1,11 +1,66 @@
-import { AntdCreateInferencer } from "@refinedev/inferencer/antd";
+import { Typography, Form, Input, InputNumber } from "antd";
+import { Create, DateField, useForm } from "@refinedev/antd";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
 
+const { Title } = Typography;
+const { TextArea } = Input;
+
 export default function NotesCreate() {
-  return <AntdCreateInferencer hideCodeViewerInProduction/>;
-}
+  const { formProps, saveButtonProps, formLoading } = useForm();
+
+  return (
+    <Create saveButtonProps={saveButtonProps} isLoading={formLoading}>
+      <Form {...formProps} layout="vertical">
+        <Form.Item
+            label="Title"
+            name="title"
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label="Content"
+            name="content"
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+        <Form.Item
+            label="Created By"
+            name="created_by"
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item
+            label="Created At"
+            name="created_at"
+            rules={[
+                {
+                    required: true,
+                },
+            ]}
+        >
+            <Input/>
+        </Form.Item>
+      </Form>
+    </Create>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
   const { authenticated, redirectTo } = await authProvider.check(context);
