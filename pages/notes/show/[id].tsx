a02c163fc632a2b6433a7b9a358a91677b9b6644
Robbob24 <r.lawrence2491@gmail.com>
2024-03-21 15:32:01 -0400

    30th

--- pages/notes/show/[id].tsx
+++ pages/notes/show/[id].tsx
@@ -1,44 +1,58 @@
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { authProvider } from "src/authProvider";
-import { DateField, ImageField, Show, TextField } from "@refinedev/antd";
-import { Card, Col, Divider, Row, Space, Typography } from "antd";
+import { DateField, ImageField, MarkdownField, Show, TextField } from "@refinedev/antd";
+import { Avatar, Card, Col, CountdownProps, Descriptions, Divider, Row, Space, Statistic, Typography } from "antd";
 import { useShow } from "@refinedev/core";
+import Comments from "@components/Comments";
+
+
 
 
 const { Title } = Typography;
 
-export default function NotesShow() {
+export default function ProjectsShow() {
   const { queryResult } = useShow();
   const { data, isLoading } = queryResult;
 
   const record = data?.data;
 
+  const { Countdown } = Statistic;
+
+const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Dayjs is also OK
+
+const onFinish: CountdownProps['onFinish'] = () => {
+  console.log('finished!');
+};
+
+const onChange: CountdownProps['onChange'] = (val) => {
+  if (typeof val === 'number' && 4.95 * 1000 < val && val < 5 * 1000) {
+    console.log('changed!');
+  }
+};
+
   return (
     <Show isLoading={isLoading}>
       <Card>
-      <Title level={1}>{record?.title ?? ""}</Title>
-      <Divider/>
-      <TextField value={record?.content} />
-      <br/><br/><br/><br/>
-      <Row gutter={16}>
-      <Col span={6} offset={18}>
-      <Title level={5}>Created At:&nbsp;<DateField
-          value={record?.created_at}
-          title="Created At"
-        /> <Title level={5}>Created By:&nbsp;<ImageField
-        value={record?.avatar_url}
-        title="Created By"
-        width={75}
-        />
-        </Title>
-        </Title>
+      <Row gutter={8}>
+        <Col span={12} flex={2} >
+        <Avatar src={record?.image ?? ""}
+                    shape="square"
+                    size={300}
+                    />
         </Col>
-        <Col>
-        <ImageField value={record?.image} width={75}/>
+
+        <Col span={12} flex={3}>
+          <Typography.Title level={3}>{record?.title}</Typography.Title>
+          <Typography.Title level={3}><TextField value={record?.content}/></Typography.Title>
+          <Typography.Title level={3}>Created By:&nbsp;<TextField value={record?.created_by}/>&nbsp; &nbsp;
+          <Avatar size={50} src={record?.avatar_url}/></Typography.Title>
+          <Typography.Title level={3}>Created At:&nbsp;<DateField value={record?.created_at}/></Typography.Title>
         </Col>
-        </Row>
         
+        
+       
+      </Row>
       </Card>
     </Show>
   );
